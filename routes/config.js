2e63d5e0ef0f3e53044f78b888eb6565394888d0
SimonCovenant <simoncovenant47@gmail.com>
2020-11-26 02:39:14 +0100

    major heroku commit

---
+++ routes/config.js
@@ -0,0 +1,5 @@
+export default {
+    CLOUDINARY_API_KEY: process.env.CLOUDINARY_API_KEY || '331536713766498',
+    CLOUDINARY_API_SECRET: process.env.CLOUDINARY_API_SECRET || 'n1qPlrdSnbQeo3cCNr6c5gkbIh4',
+    GEOCODER_API_KEY: process.env.GEOCODER_API_KEY || 'AIzaSyAf4Slpc1q2SiRh-b1w7U7LIeaJe51WF2Y'
+}
