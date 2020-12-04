cc63c77ddabec886650e3a219d6107abdaa815b4
Covenant Simon <70116851+ccurves@users.noreply.github.com>
2020-12-04 02:50:31 +0100

    Delete config.js

--- routes/config.js
+++
@@ -1,8 +0,0 @@
-export default {
-    CLOUDINARY_API_KEY: process.env.CLOUDINARY_API_KEY || '331536713766498',
-    CLOUDINARY_API_SECRET: process.env.CLOUDINARY_API_SECRET || 'n1qPlrdSnbQeo3cCNr6c5gkbIh4',
-    GEOCODER_API_KEY: process.env.GEOCODER_API_KEY || 'AIzaSyAf4Slpc1q2SiRh-b1w7U7LIeaJe51WF2Y',
-    DATABASEURL: process.env.DATABASEURL || 'mongodb://localhost/waka_dupe'
-}
-// mongodb://localhost/waka_dupe
-//mongodb+srv://simon:ccurves@wakadupe.6j6xx.mongodb.net/wakadupe?retryWrites=true&w=majority
